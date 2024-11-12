1b1e2d6fc6482c4f76082914e6d672c071fdad04
talarisulochana <talarisulochana123@gmail.com>
2024-11-12 19:56:32 +0530

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,59 @@
+
+
+for(var a=1;a<3;a++){
+    console.log(a)
+}
+
+for(var a=1;a<7;a++){
+    console.log(a)
+}
+
+for(var a=24;a>15;a--){
+    console.log(a)
+}
+
+
+
+
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++
+}
+
+var a=10;
+while(a>=6){
+    console.log(a)
+    a--
+}
+
+var i=1;
+while(i<4){
+    i++
+}
+console.log(i)
+
+
+var a=1;
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+var a=10;
+do{
+    a--
+    console.log(a)
+}while(a>=7)
+
+var a=10;
+do{
+    console.log(a)
+    a--
+}while(a>=7)
+
+var i=1;
+do{
+    i++
+    console.log(i)
+}while(i<=5)
